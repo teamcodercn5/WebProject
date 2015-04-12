77bb13f01cb562b3914aa69300293e095ce6a12a
TeamCoderCN5 <hathanhtamnd@hotmail.com>
2015-04-12 18:29:50 +0700

    1

--- qt.com.vn/WebContent/SQL/DienThoaiDiDong.sql
+++ qt.com.vn/WebContent/SQL/DienThoaiDiDong.sql
@@ -75,18 +75,18 @@ CREATE TABLE `cauhinh` (
 
 LOCK TABLES `cauhinh` WRITE;
 /*!40000 ALTER TABLE `cauhinh` DISABLE KEYS */;
-INSERT INTO `cauhinh` VALUES ('IP564GW','DVGA, 4.0\", 640 x 1136 pixels','Apple A6, 2 nhân, 1.3 GHz','1 GB','iOS 6','8.0 MP, Quay phim FullHD 1080p@30fps','1.2 MP','64 GB','Không','1440 mAh');
+INSERT INTO `cauhinh` VALUES ('IP564G','DVGA, 4.0\", 640 x 1136 pixels','Apple A6, 2 nhân, 1.3 GHz','1 GB','iOS 6','8.0 MP, Quay phim FullHD 1080p@30fps','1.2 MP','64 GB','iOS 6','1440 mAh');
 /*!40000 ALTER TABLE `cauhinh` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `gioithieusanpham`
+-- Table structure for table `gioithieu`
 --
 
-DROP TABLE IF EXISTS `gioithieusanpham`;
+DROP TABLE IF EXISTS `gioithieu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gioithieusanpham` (
+CREATE TABLE `gioithieu` (
   `masp` varchar(15) NOT NULL,
   `chitiet` text,
   PRIMARY KEY (`masp`)
@@ -94,13 +94,13 @@ CREATE TABLE `gioithieusanpham` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `gioithieusanpham`
+-- Dumping data for table `gioithieu`
 --
 
-LOCK TABLES `gioithieusanpham` WRITE;
-/*!40000 ALTER TABLE `gioithieusanpham` DISABLE KEYS */;
-INSERT INTO `gioithieusanpham` VALUES ('IP564GW','<p>&nbsp&nbsp&nbsp&nbspApple vừa chính thức giới thiệu iPhone 5 tại sự kiện diễn ra ở trung tâm hội nghị Yerba Buena. iPhone mới có chip xử lý Apple A6, thiết kế với vật liệu chính là nhôm và kính, kết nối LTE và màn hình kích thước 4”. Như vậy sau 5 năm xuất hiện trên thị trường thì Apple đã lần đầu tiên thay đổi kích thước màn hình cho iPhone. Tên gọi iPhone 5 được lấy theo thứ tự kế tiếp từ iPhone 4S và nó đồng nghĩa rằng con số đi theo tên gọi sẽ không thể hiện thế hệ của iPhone khi máy đã bước sang thế hệ thứ 6. Máy vẫn có máy ảnh độ phân giải 8MP nhưng chất lượng đã được cải thiện đáng kể, sử dụng chip Apple A6 nhỏ hơn nhưng mạnh hơn, thiết kế mỏng hơn 18% và nhẹ hơn 20% so với iPhone 4S.</p><br><p>&nbsp&nbsp&nbsp&nbspThiết kế của iPhone mới không có quá nhiều thay đổi so với iPhone 4S nhưng đã mỏng hơn và nhẹ hơn. iPhone 5 mỏng 7,6mm và nặng 112gram, nó là smartphone mỏng nhất và nhẹ nhất mà Apple từng làm. Mặt sau của máy được làm từ hợp kim nhôm thay vì mặt kính bóng như iPhone 4S trước đây. Ngoài ra, phần mặt sau cũng được chia làm ba khoang với màu đen và xám. Phần màu đen này chiếm diện tích nhỏ ở cạnh trên và dưới của máy, nếu là máy màu trắng thì mảng nhỏ này sẽ là màu trắng.</p><br><p>&nbsp&nbsp&nbsp&nbspMáy ảnh 8MP của iPhone 5 có ống kính được làm từ 5 thành phần thấu kính với độ mở lớn nhất đạt f/2.4 và có bộ lọc hồng ngoại (IR) cộng với chế độ chụp tối linh hoạt. Đáng chú ý khi Apple bổ sung khả năng chụp panorama, tính năng này đã từng xuất hiện trước đây trong một phiên bản iOS nhưng chưa được Apple kích hoạt. Ngoài cải thiện về khả năng chụp thiếu sáng, iPhone 5 sẽ có màn trập hoạt động với tốc độ nhanh hơn tới 40% so với thế hệ trước. Camera của iPhone 5 có khả năng ghi hình Full-HD trong khi camera trước với cảm biến BSI có thể quay phim HD, kết hợp với mạng LTE thì bạn hoàn toàn có những cuộc nói chuyện video chất lượng cao qua FaceTime. Một số nâng cấp khác gồm chống rung, giảm nhiễu hiệu quả hơn và khả năng chụp ảnh trong khi quay phim.</p><br><div class=\"img\"><img src=\"../../Images/Product/IP5/IP5_1.jpg\" width=\"800px\" height=\"321px\"/></div><br><p>&nbsp&nbsp&nbsp&nbspChip Apple A6 trên iPhone 5 đã được Apple giảm kích thước nhưng lại tăng sức mạnh đáng kể khi nhân xử lý và GPU (nhân đồ hoạ) đều tăng gấp đôi về hiệu năng. iPhone mới đi kèm với kết nối mới mà Apple gọi là Lightning, nó nhỏ hơn 80% với chỉ 8 chân kết nối, ổn định và nhanh hơn so với chuẩn hiện tại. Nếu bạn có một dây cáp đời cũ và muốn sử dụng với iPhone 5, Apple sẽ bán phụ kiện chuyển đổi từ 30 chân xuống 8 chân với giá chưa tiết lộ</p><br><div class=\"img\"><img src=\"../../Images/Product/IP5/IP5_2.jpg\" width=\"524px\" height=\"488px\"/></div><br><p>&nbsp&nbsp&nbsp&nbspTheo truyền thống, Apple vẫn trang bị màn hình độ phân giải Retina cho chiếc iPhone mới. Màn hình này có kích thước 4”, độ phân giải 640*1136, tỉ lệ tương đương 16:9. Với kích thước này, tỉ lệ điểm ảnh trên mỗi inch vẫn là 326ppi và người dùng vẫn dễ dàng sử dụng iPhone 5 với một bàn tay. Được phủ lớp kính Corning Gorilla Glass, màn hình trên iPhone sẽ có khả năng chống trày tuyệt vời.</p>');
-/*!40000 ALTER TABLE `gioithieusanpham` ENABLE KEYS */;
+LOCK TABLES `gioithieu` WRITE;
+/*!40000 ALTER TABLE `gioithieu` DISABLE KEYS */;
+INSERT INTO `gioithieu` VALUES ('IP564G','<p>Apple vừa chính thức giới thiệu iPhone 5 tại sự kiện diễn ra ở trung tâm hội nghị Yerba Buena. iPhone mới có chip xử lý Apple A6, thiết kế với vật liệu chính là nhôm và kính, kết nối LTE và màn hình kích thước 4”. Như vậy sau 5 năm xuất hiện trên thị trường thì Apple đã lần đầu tiên thay đổi kích thước màn hình cho iPhone. Tên gọi iPhone 5 được lấy theo thứ tự kế tiếp từ iPhone 4S và nó đồng nghĩa rằng con số đi theo tên gọi sẽ không thể hiện thế hệ của iPhone khi máy đã bước sang thế hệ thứ 6. Máy vẫn có máy ảnh độ phân giải 8MP nhưng chất lượng đã được cải thiện đáng kể, sử dụng chip Apple A6 nhỏ hơn nhưng mạnh hơn, thiết kế mỏng hơn 18% và nhẹ hơn 20% so với iPhone 4S.</p><br>\r\n<b>Thiết kế nhẹ hơn, mỏng hơn và dài hơn</b>\r\n<p>Nhìn khá giống iPhone 4S, song nếu quan sát thật gần, iPhone 5 thực sự khác biệt. Apple luôn đầu tư rất mạnh cho mảng kiểu dáng – khi cầm máy, bạn sẽ thấy mobile nhẹ hơn và mỏng hơn người tiền nhiệm. Trong khi iPhone 4S sở hữu kích thước 115,2 x 58,6 x 9,3mm và nặng 140g, thông số tương ứng trên iPhone 5 là 123,8 x 58,6 x 7,6mm và nặng 112g – rất khác biệt khi cầm trên tay.</p>\r\n<img class=\"img\" src=\"../../Images/Product/IP564GW/1.jpg\"></img>\r\n<p>Trên thực tế, màn hình 4 inch tỷ lệ 16:9 đồng nghĩa rằng, sản phẩm có thêm không gian hiển thị trong khi chiều rộng không đổi. Khe nano Sim nhìn chẳng khác mấy so với iPhone 4S, song thực tế nó đã giảm kích thước nhằm chào đón dòng sim mới. Ngoài ra, giắc cắm tai nghe 3,5mm được chuyển xuống bên dưới, nằm cạnh cổng kết nối Lightning.</p>\r\n<p>Nắp lưng của iPhone 5 sử dụng kim loại nguyên khối, giúp giảm độ dày so với lớp kính bóng bẩy trên iPhone 4S. Đồng thời, lớp mạ tinh tế bên ngoài khiến điện thoại trông đẹp mắt và thú vị hơn trước rất nhiều. </p><br>\r\n<b>Màn hình Retina 4 inch</b>\r\n<p>Sau 5 năm trung thành với màn hình 3.5 inch, cuối cùng Apple đã quyết định nâng cấp kích thước hiển thị trên iPhone 5. Với độ phân giải 1.236 x 640 pixel cho mật độ điểm ảnh tương tự iPhone 4S (326ppi), người dùng nhận được màn hình Retina với không gian hiển thị lớn hơn và phù hợp hơn khi thưởng thức phim qua màn ảnh rộng.</p>\r\n<img class=\"img\" src=\"../../Images/Product/IP564GW/2.jpg\"></img>\r\n<p>Ngoài ra, màn hình trên iPhone 5 không chỉ lớn hơn mà còn rực rỡ hơn so với iPhone 4S, nhờ mức tăng độ bão hòa màu lên tới 44%. Do đó, màu sắc trở nên cực ấn tượng và có thể khẳng định, đây là chiếc màn hình hàng đầu trong làng smartphone đương đại.</p><br>\r\n<b>Chíp xử lý A6: Nhanh hơn</b>\r\n<p>Mặc dù chưa thể thử nghiệm các ứng dụng được thiết kế cho chíp xử lý A6, song khi chơi game như Horn, Infinity Blade 2 và Racing Real 2 đều ghi nhận tốc độ tăng đáng kể so với trước. Bên cạnh đó, bạn cần lưu ý rằng, thông số kỹ thuật không phải tất cả. Quan trọng là thiết bị thể hiện thế nào trên thực tế và iPhone 5 đang làm rất tốt.Như Apple công bố, trong tương lai, các ứng dụng trên iPhone 5 sẽ ngày càng tốt hơn, load nhanh hơn và đồ họa tốt hơn – điều này được chứng thực phần nào trong các thử nghiệm gần đây.</p><br>\r\n<b>Máy ảnh tốt hơn: Hỗ trợ Panorama</b><br>\r\n<p>Mặc dù độ phân giải vẫn đạt 8MP nhưng máy ảnh trên iPhone 5 tốt hơn iPhone 4S rất nhiều. Đó là một ống kính được liên kết tốt hơn, bảo vệ bằng sapphire và tăng tốc độ chụp (khoảng 40%) nhờ chíp xử lý A6 mạnh mẽ.</p>\r\n<p>Bên cạnh đó, khả năng chụp trong điều kiện thiếu ánh sáng được cải thiện khi độ nhiễu giảm đáng kể. Đó là nhờ công nghệ kết hợp điểm ảnh mà Apple rất tâm đắc. Khả năng chỉnh sửa ảnh qua điện thoại được hỗ trợ tốt nhờ hàng núi ứng dụng trên App Store, cũng như trình chỉnh sửa cài sẵn trên máy.</p>\r\n<img class=\"img\" src=\"../../Images/Product/IP564GW/3.jpg\"></img>\r\n<p>Thêm một nét mới là chế độ chụp Panorama, cho phép ghi hình với góc mở lên đến 240 độ và kích cỡ 28 Megapixel đầy ấn tượng. Camera còn cho phép quay phim 1080p Full HD, giúp nhận diện 10 khuôn mặt khác nhau và thoải mái chụp hình ngay khi đang quay phim.</p>\r\n<p>Phía trước là camera 1.2 Megapixel mà bạn có thể tận dụng như một tấm gương hữu ích hoặc quay phim HD 720p. Tất cả các đoạn phim/ảnh chụp đều có thể chia sẻ nhanh chóng nhờ tính năng Streams mới trên iOS 6.</p><br>\r\n<b>Hệ điều hành iOS 6</b>\r\n<p>Không chỉ phần cứng, iPhone 5 còn được cài sẵn hệ điều hành iOS 6 với hơn 200 tính năng cải tiến. Siri trở nên giỏi giang hơn với khả năng hỗ trợ nhà kinh doanh hoặc cập nhật tin tức thể thao nổi bật. Facebook thì được tích hợp sâu hơn, giống như Twitter trên iOS 5 vậy.</p><br>\r\n<b>Pin lâu hơn</b>\r\n<p>Không chỉ nâng cấp tốc độ, chíp A6 ghi nhận khả năng tiết kiệm năng lượng tốt hơn trước. Khi thử nghiệm cùng iPhone 4S cho các nhiệm vụ tương tự, iPhone 5 cho thấy thời lượng pin dài hơn. Dù không thể kiểm tra chính xác các thông số được quảng cáo (như 8 giờ sử dụng 3G, 10 giờ xem video, 40 giờ nghe nhạc...), song phải khẳng định nguồn pin trên iPhone 5 rất khả quan.</p><br>\r\n<b>Kết luận</b>\r\n<p>Apple đã không giới thiệu smartphone hoàn toàn mới như nhiều người mong đợi, song iPhone 5 chắc chắn chiếm được cảm tình của những ai từng sở hữu những phiên bản iPhone trước. Đúng vậy, đây chính là sản phẩm mà khách hàng muốn nâng cấp, bởi iPhone 5 đem lại cảm giác an toàn cho chủ nhân, giúp họ biết phải sử dụng thế nào ngay sau khi mua.</p>');
+/*!40000 ALTER TABLE `gioithieu` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -129,7 +129,7 @@ CREATE TABLE `nhanvien` (
 
 LOCK TABLES `nhanvien` WRITE;
 /*!40000 ALTER TABLE `nhanvien` DISABLE KEYS */;
-INSERT INTO `nhanvien` VALUES ('nhanvien1','1234','Nhân viên 1','Nhập liệu','123456789','3000000','nhanvien1@hotmail.com','0912121212'),('nhanvien2','1234','Nhân viên 2','Nhập liệu','123123123','2500000','nhanvien2@hotmail.com','0912313123');
+INSERT INTO `nhanvien` VALUES ('nhanvien1','1234','Nhân viên 1','Nhập liệu','123456789','3000000','nhanvien1@hotmail.com','0913131313');
 /*!40000 ALTER TABLE `nhanvien` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +158,7 @@ CREATE TABLE `sanpham` (
 
 LOCK TABLES `sanpham` WRITE;
 /*!40000 ALTER TABLE `sanpham` DISABLE KEYS */;
-INSERT INTO `sanpham` VALUES ('IP564GW','IPhone 5 64GB White','6.350.000 VNĐ','12 tháng phần mềm','Sạc cáp tai nghe cao cấp','Mới 99%');
+INSERT INTO `sanpham` VALUES ('IP564G','IPhone 5 64GB','6.350.000 VNĐ','12 tháng phần mềm','Sạc cáp tai nghe cao cấp','Mới 99%'),('IP664G','Iphone 6 Plus 64GB','20.790.000 VNĐ','12 tháng phần cứng, 12 tháng phần mềm','Fullbox: Sạc, Cáp, Tai Nghe','Mới 100%');
 /*!40000 ALTER TABLE `sanpham` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-04 19:28:34
+-- Dump completed on 2015-04-12 18:29:13
